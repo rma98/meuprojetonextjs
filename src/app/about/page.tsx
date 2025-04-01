ee81398916229a3cc2c197a2b1007d69bae73bb1
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-04-01 19:16:53 -0300

    Add page.tsx file for folder about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
+            <h1 className="text-4xl font-bold text-green-600">Sobre Nós</h1>
+            <p className="mt-4 text-lg text-gray-700 text-center">
+                Esta é a página Sobre. Aqui falamos mais sobre nosso projeto.
+            </p>
+        </main>
+    );
+}
