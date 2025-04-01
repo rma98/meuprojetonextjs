1ad6d3c6eb78276ad0bd97f03c4593bef5395238
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-04-01 19:17:13 -0300

    Add page.tsx file for folder contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
+            <h1 className="text-4xl font-bold text-red-600">Contato</h1>
+            <p className="mt-4 text-lg text-gray-700 text-center">
+                Entre em contato conosco para mais informações!
+            </p>
+        </main>
+    );
+}
